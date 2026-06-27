334c8c298f67068e852cee776b0b4b7f0ed6261f
RidwanRosling <ridwansbdt@gmail.com>
2026-06-27 15:43:35 +0700

    test

--- resources/views/tentang.blade.php
+++ resources/views/tentang.blade.php
@@ -3,12 +3,12 @@
 @section('title', 'About | Nginapin')
 
 @section('content')
-<x-main-root>
-    <div class="mx-auto max-w-3xl px-4 py-16">
-        <h1 class="text-4xl font-bold text-neutral-900">Tentang Nginapin</h1>
-        <p class="mt-4 text-lg text-neutral-600">
-            Nginapin adalah platform untuk mencari kosan dan kontrakan di Bogor.
-        </p>
-    </div>
-</x-main-root>
+    <x-main-root>
+        <div class="mx-auto max-w-3xl px-4 py-16">
+            <h1 class="text-4xl font-bold text-neutral-900">Tentang Nginapin</h1>
+            <p class="mt-4 text-lg text-neutral-600">
+                Nginapin adalah platform untuk mencari kosan dan kontrakan di Bsdsaogor.
+            </p>
+        </div>
+    </x-main-root>
 @endsection
